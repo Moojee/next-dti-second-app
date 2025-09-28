f66543521c4b5dd4fbd00452a0e8017554a4ebd6
moojee <sutitamoojee@gmail.com>
2025-09-28 17:48:41 +0700

    edit majoe, font, footer, bla ๆ ๆ

--- app/major/page.tsx
+++ app/major/page.tsx
@@ -1,15 +1,34 @@
 import Menumajor from "@/components/menumajor";
 import Menubar from "@/components/menubar";
+import { Itim, Bungee } from "next/font/google";
+
+const itim = Itim({
+  subsets: ["thai"],
+  weight: ["400"],
+});
+
+const bungee = Bungee({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 export default function Page() {
   return (
     <>
-    <Menubar/>
-    <Menumajor/>
-     <h1 className="text-center" >Major</h1>
-     <hr />
-     <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse accusantium aperiam earum animi voluptatem, et mollitia molestias laboriosam est laudantium sunt aspernatur deserunt illum iste aliquam voluptas ea reprehenderit! Veritatis!
-    </p>
+      <Menubar />
+      <Menumajor />
+      <h1 className={`${itim.className} text-center`}>
+        {" "}
+        สาขาวิชาต่าง ๆ ในมหาวิทยาลัย
+      </h1>
+      <hr />
+      <br />
+      <p className={`${bungee.className}`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+        accusantium aperiam earum animi voluptatem, et mollitia molestias
+        laboriosam est laudantium sunt aspernatur deserunt illum iste aliquam
+        voluptas ea reprehenderit! Veritatis!
+      </p>
     </>
   );
 }
