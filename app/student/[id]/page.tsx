2e44232e0b2abd5a2c47b78757ce177856845e68
moojee <sutitamoojee@gmail.com>
2025-09-28 17:18:53 +0700

    edit params

--- app/student/[id]/page.tsx
+++ app/student/[id]/page.tsx
@@ -1,11 +1,17 @@
-export default function Page({params}:{params: {id: string} }) {
-    const {id} = params;
+export default async function Page({ params }: { params: { id: string } }) {
+  // สามารถใช้ async ได้ ถ้าในนี้มี fetch
+  const { id } = params;
 
   return (
     <>
-     <h1>Student ID : {id} </h1> 
-     <hr />
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatibus voluptas amet quas voluptate facere commodi repellat excepturi. Nulla quae excepturi enim ipsam necessitatibus omnis? Quibusdam nobis alias neque reiciendis.</p>
+      <h1>Student ID : {id}</h1>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptatibus
+        voluptas amet quas voluptate facere commodi repellat excepturi. Nulla quae
+        excepturi enim ipsam necessitatibus omnis? Quibusdam nobis alias neque
+        reiciendis.
+      </p>
     </>
   );
 }
